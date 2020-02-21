e07afa92b072aa6308dab60d990f862cd14ed77c
macbook <1111>
2020-02-21 20:16:53 +0900

    dfs 구현함수 작성중

---
+++ dataStructure/src/dfsAlgorithm.java
@@ -0,0 +1,61 @@
+public class dfsAlgorithm {
+
+    //그래프(인접행렬)클래스
+    class DfsGraph {
+        private int nV; // 정점의 개수
+        private int[][] dfsGraph; // 그래프
+        private boolean[] visitArr; // 정점 방문 여부 확인 배열
+
+        //그래프 초기화
+        public DfsGraph(int nV) {
+            this.nV = nV;
+
+            /*
+            put(int x, int y)에서 입력되는 정점의 값은 0 이상의 정수이나
+            배열의 index는 0부터 시작이므로
+            정점을 담는 인접행렬의 행과 열 size는 1을 더하여 초기화해줌
+             */
+            this.dfsGraph = new int[this.nV+1][this.nV+1];
+
+            this.visitArr = new boolean[this.nV+1];
+        }
+
+        //그래프 return
+        public int[][] getGraph(){
+            return this.dfsGraph;
+        }
+
+        //그래프 추가(양방향)
+        public void put(int x, int y){
+            this.dfsGraph[x][y] = this.dfsGraph[y][x];
+        }
+
+        //그래프 추가(단방향)
+        public void putSingle(int x, int y){
+            this.dfsGraph[x][y] = 1;
+        }
+
+        //그래프 출력(인접행렬)
+        public void printGraphToAdjArr() {
+            for(int i=0; i<this.dfsGraph.length; i++){
+                for (int j=0; j<this.dfsGraph[i].length; i++){
+                    System.out.print("" + this.dfsGraph[i][j]);
+                }
+                System.out.println();
+            }
+        }
+
+        //정점 방문 여부 확인 배열 초기화
+        public void clearVisitArr() {
+            for (int i=0; i<this.visitArr.length; i++) {
+                this.visitArr[i] = false;
+            }
+        }
+
+        //그래프 탐색(재귀호출)
+        public void dfs(int vIdx){
+
+        }
+    }
+
+}
